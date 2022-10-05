458c437cbfe413543424b2d44c28d9e1e5d9aa31
JamalMydeen <115083239+jamAL108@users.noreply.github.com>
2022-10-05 18:04:03 +0530

    Create percentage.java

---
+++ percentage.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class expt1
+{
+ public static void main(String[] args)
+ {
+	 Scanner n = new Scanner(System.in);
+	 System.out.println("enter the marks:");
+	 int a=n.nextInt();
+	 a=a/10;
+ switch(a)
+  {
+	 case 0:
+	 System.out.println("Fail");
+	 break;
+	 case 1:
+	 System.out.println("Fail");
+	 break;
+	 case 2:
+	 System.out.println("Fail");
+	 break;
+	 case 3:
+	 System.out.println("Fail");
+	 break;
+	 case 4:
+	 System.out.println("pass");
+	 break;
+	 case 5:
+	 System.out.println("second class");
+	 break;
+	 case 6:
+	 System.out.println("First class");
+	 break;
+	 case 7:
+	 System.out.println("destinction");
+	 break;
+	 case 8:
+	 System.out.println("destinction");
+	 break;
+	 case 9:
+	 System.out.println("destinction");
+	 break;
+	 case 10:
+	 System.out.println("destinction");
+	 break;
+  }
+ }
+}
